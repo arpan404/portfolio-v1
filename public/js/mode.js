b6f89a13c0b3be11900ebafcde77ec64bb95d456
arpan404 <itsmearpan1@gmail.com>
2023-03-03 23:27:38 +0545

    done

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,8 +1,10 @@
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const userTheme = localStorage.getItem("theme");
 const themeSet = () =>{
-  if(userTheme==="dark" || (!userTheme && systemTheme)){
+  console.log(userTheme ==="dark" || (!userTheme && systemTheme));
+  if(userTheme ==="dark" || (!userTheme && systemTheme)){
     document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
     return;
   }
 }
@@ -13,9 +15,10 @@ const colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
 const setColorScheme = e => {
   if (e.matches) {
     document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
   } else {
     document.documentElement.classList.remove("dark");
-
+    localStorage.setItem("theme", "light");
   }
 }
   
