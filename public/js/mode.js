a80bc7c022ce7199b480b99d745ab4b1df10518f
arpan404 <itsmearpan1@gmail.com>
2023-03-24 18:58:19 +0545

    dark mode fix

--- public/js/mode.js
+++ public/js/mode.js
@@ -1,18 +1,17 @@
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 const userTheme = localStorage.getItem("theme");
-const themeSet = () =>{
-  console.log(userTheme ==="dark" || (!userTheme && systemTheme));
-  if(userTheme ==="dark" || (!userTheme && systemTheme)){
+const themeSet = () => {
+  console.log(userTheme === "dark" || (!userTheme && systemTheme));
+  if (userTheme === "dark" || (!userTheme && systemTheme)) {
     document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
     return;
   }
-}
+};
 themeSet();
 
-const colorSchemeQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+const colorSchemeQueryList = window.matchMedia("(prefers-color-scheme: dark)");
 
-const setColorScheme = e => {
+const setColorScheme = (e) => {
   if (e.matches) {
     document.documentElement.classList.add("dark");
     localStorage.setItem("theme", "dark");
@@ -20,7 +19,6 @@ const setColorScheme = e => {
     document.documentElement.classList.remove("dark");
     localStorage.setItem("theme", "light");
   }
-}
-  
-setColorScheme(colorSchemeQueryList);
-colorSchemeQueryList.addEventListener('change', setColorScheme);
+};
+
+colorSchemeQueryList.addEventListener("change", setColorScheme);
